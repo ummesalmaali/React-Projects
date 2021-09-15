476ce743dee1e1a596c828c68233327f8da7de7e
ummesalmaali <ummes3527@gmail.com>
2021-09-15 07:03:13 +0600

    filtered

---
+++ src/components/Basics/MenuCard.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function MenuCard({ menuData }) {
+  console.log(menuData);
+  return (
+    <>
+      <section className="main-card--cointainer">
+        {menuData.map((curEle) => {
+          return (
+            <div>
+              <div className="card-container" key={curEle.id}>
+                <div className="card">
+                  <div className="card-body">
+                    <span className="card-number card-circle subtle">
+                      {curEle.id}
+                    </span>
+                    <span className="card-author subtle">{curEle.name}</span>
+                    <h2 className="card-title">{curEle.name}</h2>
+                    <span className="card-description subtle">
+                      {curEle.description}
+                    </span>
+                    <div className="card-read">Read</div>
+                  </div>
+                  <img src={curEle.image} alt="food" className="card-media" />
+                  <span className="card-tag subtle">Order Now</span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </>
+  );
+}
--- src/components/Basics/Restaurant.js
+++ src/components/Basics/Restaurant.js
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.css";
+// import image from "../../image/b1.jpg";
+import Menu from "./menuApi";
+import MenuCard from "./MenuCard";
 
 export default function Restaurant() {
+  const [menuData, setMenuData] = useState(Menu);
+  const filterItem = (category) => {
+    const updatedList = Menu.filter((curEle) => {
+      return curEle.category === category;
+    });
+    setMenuData(updatedList);
+  };
+  //   const filterLunchItem = (category) => {
+  //     const updatedLunch = Menu.filter((currEle) => {
+  //       return currEle.category === category;
+  //     });
+  //     setMenuData(updatedLunch);
+  //   };
+
+  //   const filterEvening = (category) => {
+  //     const updatedEvening = Menu.filter((curEle) => {
+  //       return curEle.category === category;
+  //     });
+  //     setMenuData(updatedEvening);
+  //   };
+  //   const filterDinner = (category) => {
+  //     const updatedDinner = Menu.filter((curEle) => {
+  //       return curEle.category === category;
+  //     });
+  //     setMenuData(updatedDinner);
+  //   };
+
   return (
-    <div>
-      <h1 className="rest">hello restaurant</h1>
-    </div>
+    <>
+      <nav className="navbar">
+        <div className="btn-group">
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("breakfast")}
+          >
+            Breakfast
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => {
+              filterItem("lunch");
+            }}
+          >
+            Lunch
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("evening")}
+          >
+            Evening
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("dinner")}
+          >
+            Dinner
+          </button>
+          <button className="btn-group__item" onClick={() => setMenuData(Menu)}>
+            All
+          </button>
+        </div>
+      </nav>
+      <MenuCard menuData={menuData} />
+    </>
   );
 }
